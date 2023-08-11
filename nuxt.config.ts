92c1093328016743cae57881649c3b5ca0fb824b
alexispace <papadi166@gmail.com>
2023-08-11 21:58:40 +0200

    prerender fix?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,7 +38,7 @@ export default defineNuxtConfig({
   nitro: {
     prerender: {
       crawlLinks: true,
-      routes: ["/", "/sitemap.xml"],
+      routes: ["/"],
     },
   },
 
---
+++ storyblok/Page.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+div
+    div(v-editable="blok")
+</template>
+
+<script setup lang="ts">
+defineProps({
+  blok: {
+    type: Object,
+    required: false,
+  },
+});
+</script>
+
+<style lang="scss"></style>
