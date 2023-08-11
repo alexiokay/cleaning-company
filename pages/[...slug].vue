abe17f5452e78a96c9885cd32044d4c8bf36f756
alexispace <papadi166@gmail.com>
2023-08-11 23:22:28 +0200

    .

---
+++ pages/[...slug].vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+div
+    StoryblokComponent(v-if="story" :blok="story.content")
+
+</template>
+
+<script setup lang="ts">
+const { slug } = useRoute().params;
+
+const story = await useAsyncStoryblok(
+  slug && slug.length > 0 ? slug.join("/") : "home",
+  {
+    version: "draft",
+  }
+);
+</script>
+
+<style lang="scss"></style>
