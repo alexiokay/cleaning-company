19d39b2f7d07318005c910f5efdf7745f6fff39d
alexispace <papadi166@gmail.com>
2023-08-12 22:44:14 +0200

    services page improoved

---
+++ components/Process.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+div#cart-process(class="flex h-auto w-full items-center justify-center sm:justify-between md:justify-center md:gap-x-16")
+   
+    div( class="flex h-full justify-center sm:w-auto w-auto relative")
+        div(v-for="(val, index, key) in props.steps" class="w-[5rem] sm:w-[8rem] relative md:w-[9.5rem] h-[4rem]  flex flex-col justify-between items-center")
+            button(@click="goToStep(index)" class="w-10 h-10 rounded-full items-center justify-center flex " :class="{ 'bg-gray-500 text-white': index < currentStep, 'bg-white border-[1px] border-gray-400': index > currentStep, 'bg-black text-white': index === currentStep }")
+                p(v-if="index >=currentStep") {{ index+1 }}
+                IconDone(v-if="index < currentStep")
+            p(class="text-xs text-gray-500 text-center") {{val}}
+       
+            hr(v-if="index !== props.steps.length -1" class="absolute top-[0.95rem] left-[3.7rem] sm:left-[5.6rem] md:left-[6.2rem] w-[2.7rem] sm:w-[5rem] md:w-[6.5rem] h-[1.5px] bg-gray-800")
+ 
+            
+           
+</template>
+
+<script setup lang="ts">
+import IconDone from "~icons/material-symbols/done-rounded";
+
+const route = useRoute();
+const props = defineProps<{ steps: string[] }>();
+const currentStep = computed(() => {
+  if (route.name === "services") return 0;
+  if (route.path === "/cart/order") return 1;
+  if (route.path === "/cart/payment") return 2;
+  if (route.path === "/cart/ready") return 3;
+});
+const router = useRouter();
+const goToStep = (index: number) => {
+  if (index === 0) {
+    router.push("/cart");
+  }
+  if (index === 1) {
+    router.push("/cart/order");
+  }
+};
+</script>
+
+<style lang="sass"></style>
--- pages/services.vue
+++ pages/services.vue
@@ -1,16 +1,21 @@
 <template lang="pug">
-div(class="w-full h-screen flex flex-col items-center  ")
-    div(class="w-full h-auto flex items-center justify-center bg-slate-100 py-12")
+div(class="w-full h-screen flex flex-col items-center gap-y-4  ")
+    div(class="w-full relative h-auto flex items-center justify-center bg-slate-100 py-12")
+        button(class="absolute left-4 bg-white px-4 py-2 rounded-xl shadow-md ") Personalize
         Searchbar(class="w-3/5 h-[5rem]")
-    h2(class="text-5xl mt-12") Categories
-    div(class="w-3/5  h-auto py-12 flex ")
+    h1 Find best service provider
+    Process(:steps='["Category", "2", "3", "4"]' )
+    h2(class="text-5xl mt-12 font-semibold") Categories
+    div(class="w-3/5  h-auto py-12 flex flex-wrap gap-y-4  mt-2")
         
-        div.services__category(class=" flex flex-col items-center w-1/6 font-semibold gap-y-2")
-            nuxt-img(src="icons/icon_23.png" width="500" height="500" class="object-cover  aspect-square w-[8rem]" alt="icon" title="icon" format="webp")
+        div.services__category(class=" flex flex-col items-center w-1/6 font-semibold gap-y-2" v-for="i in 12")
+            nuxt-img(src="icons/icon_23.png" width="500" height="500" class="object-cover  aspect-square w-[6rem]" alt="icon" title="icon" format="webp")
             p(class="text-xl") Kitchen
         div.services__category(class=" flex flex-col items-center w-1/6 font-semibold gap-y-2")
-            nuxt-img(src="icons/icon_33.png" width="500" height="500" class="object-cover  aspect-square w-[8rem]" alt="icon" title="icon" format="webp")
+            nuxt-img(src="icons/icon_33.png" width="500" height="500" class="object-cover  aspect-square w-[6rem]" alt="icon" title="icon" format="webp")
             p(class="text-xl") Bathroom
+    div(class="w-full xl:px-[20rem] relative h-auto flex flex-col items-center justify-center bg-slate-100 py-12")
+        div(class="w-full h-[10rem] bg-white px-10 py-8 rounded-2xl") p s
 </template>
 
 <script setup lang="ts"></script>
