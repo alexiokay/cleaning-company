0166d69d8d1f086b49527cd636a92021c115bc86
alexispace <papadi166@gmail.com>
2023-08-15 03:12:50 +0200

    pricing page responsiveness improoved

--- pages/pricing.vue
+++ pages/pricing.vue
@@ -1,14 +1,14 @@
 <template lang="pug">
 div(class="w-full min-h-screen flex flex-col bg-fixed bg-top-0   bg-no-repeat bg-cover  bg-right md:bg-center  " style="background-image:url('https://uploads-ssl.webflow.com/60ff934f6ded2d17563ab9dd/63c6de6ee5753441a08d5616_01.17.23-CLEAN-OFFICE-BLOG.jpg');")
-    div.pricing__header(class=" w-full h-[18rem] flex justify-center items-center backdrop-brightness-50 px-[30%]  ")
+    div.pricing__header(class=" w-full h-[13rem] md:h-[18rem] flex justify-center items-center backdrop-brightness-50 px-[30%]  ")
         div(class="text-white gap-y-4 flex flex-col items-start text-start w-full  font-flamabook")
             p(class="text-5xl font-bold ") Pricing
             p Home / 
                 span(class="text-blue-400") Pricing
-    div(class="min-h-screen bg-white w-full flex flex-col items-center gap-y-4 py-12 ")
+    div(class="min-h-screen bg-white w-full flex flex-col items-center gap-y-4 py-12  px-4 lg:px-[20%]")
         h1(class="text-4xl font-bold ") Deep clean / Move Out Cleaning
-        div.pricing__content(class="flex w-full px-[20%] gap-x-8 bg-white items-center justify-center mx-auto ")
-            div.pricing_content_tab(class="w-1/4 border-[1px] h-auto shadow-sm bg-white items-center flex flex-col pb-8" v-for="i in 4")
+        div.pricing__content(class="flex w-full  gap-x-8 gap-y-12 bg-white items-center md:justify-center justify-around mx-auto flex-wrap md:flex-nowrap")
+            div.pricing_content_tab(class="w-[calc(50%-1rem)] md:w-1/4 border-[1px] h-auto shadow-sm bg-white items-center flex flex-col pb-8" v-for="i in 4")
                 div(class="w-full h-[8rem] bg-[#Fcf2ec] flex items-center justify-center")
                     p(class="text-2xl font-bold") 1 Bedroom
                 div.pricing_content_tab_request(class="w-full h-auto py-4 pb-8 gap-y-2 items-center justify-center flex flex-col")
