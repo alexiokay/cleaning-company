b724d4af14c112caa0ba39060432092b5d7f4ef4
alexispace <papadi166@gmail.com>
2023-08-19 13:15:29 +0200

    trying fix

--- components/Landing/CallAssistant.vue
+++ components/Landing/CallAssistant.vue
@@ -3,8 +3,9 @@ div(class="w-full h-[40rem] pt-12 flex flex-col ")
     div(class="w-full flex justify-around h-full")
         p Hi I'am fresh and tidy.
             span Your cleaning service customization helper
+        nuxt-img(src="images/call-assistant.png"   alt="call assistant"  format="webp"  class="w-1/2 h-auto object-contain")
         
-        
+        img(src="images/call-assistant.png")
       
 </template>
 
