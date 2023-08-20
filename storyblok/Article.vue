c42f33ac94725d52b0453b2e8e29eaac8319f0e7
alexispace <papadi166@gmail.com>
2023-08-20 11:44:32 +0200

    blog fixed

--- storyblok/Article.vue
+++ storyblok/Article.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 div(class="w-full h-auto flex flex-col items-center justify-center ")
     div(v-editable="blok" class="w-full items-center justify-center flex flex-col gap-y-6 text-center")
-        nuxt-img(:src="'https:' + blok.image"  provider="" class="object-cover h-[30rem] w-full" :alt="blok.image.alt" :title="blok.title" :width="blok.width" :height="blok.height" format="webp")
+        nuxt-img(:src="'https:' + blok.image"  provider="storyblok" class="object-cover h-[30rem] w-full" :alt="blok.image.alt" :title="blok.title" :width="blok.width" :height="blok.height" format="webp")
 
         div(class="container mx-auto mb-12 text-6xl") {{ blok.title }}
         div(class="container mx-auto mb-12 text-3xl text-start leading-[2.9rem] w-4/5") {{ blok.description }}
