8e11befbd6a76cca9f7f5c888bf33aaa0ff307b5
alexispace <papadi166@gmail.com>
2023-09-12 03:06:08 +0200

    styling process

--- components/ServiceCustomatization/Progress.vue
+++ components/ServiceCustomatization/Progress.vue
@@ -1,13 +1,14 @@
 <template lang="pug">
-div(class="w-full h-auto flex flex-col  gap-y-4 items-center justify-center py-8 px:8 md:px-[15%] 4xl:px-[25%] ")    
-    div( class="w-full md:h-[20rem] gap-y-4 flex flex-col px-6 py-4 shadow-lg  items-center justify-between rounded-xl bg-white overflow-clip border-[1px]" )
-        
+div(class="w-full h-auto  flex flex-col  gap-y-4 items-center justify-center py-8 px:8 md:px-[15%] 4xl:px-[25%] bg-[#Fcf2ec]")    
+    div( class="w-full md:h-[20rem]  gap-y-4 flex flex-col px-6 py-4 shadow-lg  items-center justify-between rounded-xl bg-white overflow-clip border-[1px]" )
+        CloseIcon(@click="$emit('closeProgress')" class=" md:hidden absolute z-50 top-4 right-4 ml-auto text-2xl hover:cursor-pointer w-6 md:w-auto h-6 md:h-auto text-gray-500" ) 
+
         div.progress-bar(class="w-[calc(100%+3rem)] h-[0.25rem] bg-white -mt-4 "  )
                 div.progress-bar__bar(:style="{ width: progressBarLength }" class=" h-full bg-green-500")
        
         p(class="text-2xl text-gray-700 font-semibold tracking-wider") {{activeStep.question}}
     
-        div(class="flex flex-col gap-y-4 md:flex-row w-full justify-center items-center gap-x-6")
+        div(class="flex flex-col gap-y-2 md:flex-row w-full justify-center items-center gap-x-6")
             div.item(@click="makeAnswer(answer)" v-for="answer in activeStep.answers" :key="answer" :class="answer.answered === true? 'border-orange-500': 'border-slate-100  hover:border-orange-500'" class="rounded-lg w-full md:w-1/5 h-full py-6 md:py-14 border-[2px] flex flex-col items-center justify-center hover:cursor-pointer ")
                 p.w-full.text-center {{answer.title}}
         button(@click="nextStep" class="w-auto h-[3rem]  bg-orange-300 rounded-md px-6 py-2 text-lg font-bold text-white hover:bg-orange-500") Next
@@ -15,6 +16,8 @@ div(class="w-full h-auto flex flex-col  gap-y-4 items-center justify-center py-8
 </template>
 
 <script setup lang="ts">
+import CloseIcon from "~icons/carbon/close-outline";
+
 const props = defineProps({
   steps: {
     type: Object,
