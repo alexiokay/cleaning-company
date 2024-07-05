5b16e4c2bf7b53e2e0108c48e9a2416c072f546a
alexispace <papadi166@gmail.com>
2024-07-05 15:35:36 +0200

    selectDate component logic and style update

--- components/Custom/SelectDate.vue
+++ components/Custom/SelectDate.vue
@@ -1,47 +1,46 @@
 <template lang="pug">
 .calendar-box.w-full
-  div#dateInput(type="text", placeholder="Select a date", @click="toggleCalendar",  class="w-full p-2 border border-gray-300 rounded text-base cursor-pointer")
-    
+  div#dateInput(type="text", placeholder="Select a date", @click="toggleCalendar", class="w-full p-2 border border-gray-300 rounded text-base cursor-pointer select-none")
     p(v-if="!formattedDate") Select a date
     p(v-else) {{ formattedDate }}
   //- Overlay
   //- ClientOnly
   //-   Teleport(to="#teleportDateDropdownHere")
-  //-     div(v-show="showCalendar", ref="calendarOverlay" class="absolute  top-0 left-0 inset-0 bg-black bg-opacity-50 z-50", @click="toggleCalendar")
+  //-     div(v-show="showCalendar", ref="calendarOverlay", class="absolute top-0 left-0 inset-0 bg-black bg-opacity-50 z-50", @click="toggleCalendar")
 
-  .calendar(v-if="showCalendar", @click.stop, class="mt-[4rem] absolute top-0 left-0 w-full h-auto md:w-auto bg-white rounded-xl shadow-lg z-50 p-2 gap-y-2 flex flex-col")
-    .header.flex.justify-between.items-center.px-3.py-2.text-black.rounded-t
-      button(@click="changeMonth(-1)", class="bg-transparent border-none cursor-pointer text-black text-xl")
-        | ‹
-      h2.text-xl.font-bold {{ monthYear }}
-      button(@click="changeMonth(1)", class="bg-transparent border-none cursor-pointer text-black text-xl")
-        | ›
-    .days-names-wrapper(class="bg-[#f5f5f5] -mt-2")
-      .days-names.flex.flex-wrap
-        .day-name(v-for="dayName in dayNames", :key="dayName", class="flex-1 py-3 text-center text-xs uppercase text-black")
-          | {{ dayName }}
-    .days.grid.grid-cols-7.gap-1.py-2(class="-mt-2")
-      .day( v-for="item in daysOfMonth", :key="item.day", :class="{'bg-[#4e37e3] text-white': isSelectedDay(item.day), 'text-gray-500 cursor-not-allowed': item.isPast, 'hover:bg-gray-100': !item.isPast && !isSelectedDay(item.day)}", @click="selectDay(item.day)", class="p-2 text-center cursor-pointer rounded transition-colors")
-        p() {{ item.day }}
-      
-    div(class="w-full flex gap-x-3 ")
-
-      input(type="time" v-model="startTime" class="w-2/3 p-2 border border-gray-300 rounded text-base cursor-pointer")
-      div(class="w-1/3 flex flex-col")
-        p {{ formattedDate }}
-        p {{ startTime }}
-
-    div(class="w-full flex justify-between ")
-      button(@click="showCalendar = false", class="border-[#4e37e3] border-[1px] text-[#4e37e3] py-2 px-4 rounded-md")
-        | Cancel
-      button(@click="showCalendar = false", class="bg-[#4e37e3] text-white py-2 px-4 rounded")
-        | Apply
-
+  .calendar(v-if="showCalendar", @click.stop, class="mt-[4rem] absolute top-0 left-0 w-full h-auto md:w-auto bg-white rounded-xl drop-shadow-lg z-50 px-2 pb-2 select-none")
+    div(class="gap-y-0 flex flex-col")
+      .header.flex.justify-between.items-center.px-3.py-2.text-black.rounded-t.gap-x-2
+        button(@click="changeMonth(-1)", class="bg-transparent border-none cursor-pointer text-black text-xl hover:bg-[#f5f5f5] px-3 py-1 rounded-sm")
+          | ‹
+        div(class="hover:cursor-pointer hover:bg-[#f5f5f5] w-full text-center py-1 rounded-sm")
+          h2.text-xl.font-bold {{ monthYear }}
+        button(@click="changeMonth(1)", class="bg-transparent border-none cursor-pointer text-black text-xl hover:bg-[#f5f5f5] px-3 py-1 rounded-sm")
+          | ›
+      .days-names-wrapper(class="bg-[#f5f5f5] mb-2")
+        .days-names.flex.flex-wrap
+          .day-name(v-for="dayName in dayNames", :key="dayName", class="flex-1 py-3 text-center text-xs uppercase text-black")
+            | {{ dayName }}
+      .days.grid.grid-cols-7.gap-1.px-2(class="")
+        .day(v-for="item in daysOfMonth", :key="item.day || item.placeholder", )
+          p(v-if="!item.placeholder" :class="{'bg-[#4e37e3] text-white': isSelectedDay(item.day), 'text-gray-500 cursor-not-allowed  ': item.isPast, 'hover:bg-gray-100 cursor-pointer': !item.isPast && !isSelectedDay(item.day)}", @click="selectDay(item.day)", class="p-2 text-center  rounded transition-colors") {{ item.day || '' }}
+          p(v-else class="hover:cursor-default") &nbsp;
+      div(class="w-full flex gap-x-3 items-center mt-2")
+        input(type="time" v-model="startTime" class="w-2/3 p-2 border border-gray-300 rounded text-base cursor-pointer")
+        div(class="w-1/3 flex flex-col")
+          p {{ formattedDate }}
+          p {{ startTime }}
+      div(class="w-full flex justify-between mt-2")
+        button(@click="showCalendar = false", class="border-[#4e37e3] border-[1px] text-[#4e37e3] py-2 px-4 rounded-md")
+          | Cancel
+        button(@click="showCalendar = false", class="bg-[#4e37e3] text-white py-2 px-4 rounded")
+          | Apply
 </template>
 
 <script setup lang="ts">
 import { useBookFormStore } from "@/stores/BookForm";
 import { storeToRefs } from "pinia";
+import { ref, computed } from "vue";
 
 const showCalendar = ref(false);
 const selectedDate = ref<Date | null>(null);
@@ -57,10 +56,14 @@ function toggleCalendar() {
 }
 
 function changeMonth(offset: number) {
-  currentDate.value.setMonth(currentDate.value.getMonth() + offset);
+  currentDate.value = new Date(
+    currentDate.value.getFullYear(),
+    currentDate.value.getMonth() + offset,
+    1
+  );
 }
 
-const dayNames = ["S", "M", "T", "W", "T", "F", "S"]; // Adjust according to your start day of week preference
+const dayNames = ["M", "T", "W", "T", "F", "S", "S"]; // Adjust according to your start day of week preference
 
 const monthYear = computed(
   () =>
@@ -87,6 +90,17 @@ const daysOfMonth = computed(() => {
     1
   );
   const days = [];
+
+  // Get the day of the week the month starts on, adjusted for Monday start
+  let firstDayOfWeek = start.getDay();
+  firstDayOfWeek = firstDayOfWeek === 0 ? 6 : firstDayOfWeek - 1;
+
+  // Add placeholders for the days before the first day of the month
+  for (let i = 0; i < firstDayOfWeek; i++) {
+    days.push({ placeholder: true });
+  }
+
+  // Add actual days of the month
   while (start.getMonth() === currentDate.value.getMonth()) {
     const dayDate = new Date(start);
     days.push({
@@ -95,6 +109,7 @@ const daysOfMonth = computed(() => {
     });
     start.setDate(start.getDate() + 1);
   }
+
   return days;
 });
 
@@ -109,19 +124,10 @@ const selectDay = (day: number) => {
     day
   );
   startDay.value = selectedDate.value;
-  formattedDate.value = selectedDate.value.toLocaleDateString("en-US");
+  formattedDate.value = selectedDate.value.toLocaleDateString("en-NL");
   console.log(selectedDate.value);
   //showCalendar.value = false;
 };
-
-onMounted(() => {
-  const mainFormComponent = document.getElementById("main-form-component");
-  if (calendarOverlay.value && mainFormComponent) {
-    // Set size to cover the entire component
-    calendarOverlay.value.style.width = mainFormComponent.offsetWidth + "px";
-    calendarOverlay.value.style.height = mainFormComponent.offsetHeight + "px";
-  }
-});
 </script>
 
 <style lang="scss"></style>
